7d271d0d66ec5d6a4ea26c1b5a5fe54c6421ca19
sd576 <sd576@btinternet.com>
2020-04-25 12:24:39 +0100

    Initial Commit

---
+++ DA1.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DA1.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DA1.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ DA1.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DA1.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+} 
