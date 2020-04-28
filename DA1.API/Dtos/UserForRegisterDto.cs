2398c19df9c19e0044977a6a5957a7f77637724c
sd576 <sd576@btinternet.com>
2020-04-28 14:56:39 +0100

    sort it

--- DA1.API/Dtos/UserForRegisterDto.cs
+++ DA1.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DA1.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
