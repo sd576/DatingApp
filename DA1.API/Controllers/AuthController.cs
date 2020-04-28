e6cce31805a989534d53e02adbf80f00ab4b2912
sd576 <sd576@btinternet.com>
2020-04-28 11:37:34 +0100

    commit

---
+++ DA1.API/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks;
+using DA1.API.Data;
+using DA1.API.Dtos;
+using DA1.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DA1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validate request
+
+            userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+
+            if (await _repo.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+        } 
+    }
+}
---
+++ DA1.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,8 @@
+namespace DA1.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
